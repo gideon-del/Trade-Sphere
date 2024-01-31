30ceb7dff3dc8ac94573c311060add1e0e4bb030
gideon-del <gideonchidi471@gmail.com>
2024-01-31 15:19:51 +0100

    chore: added view product

---
+++ package/trade-sphere/app/vendors/product/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { TABLES } from "@/lib/constants";
+import superbase from "@/lib/superbase";
+import React, { useEffect } from "react";
+
+const Products = () => {
+  useEffect(() => {
+    const getProducts = async () => {
+      const products = await superbase.from(TABLES.product).select(`
+        *, 
+        image 
+        (id, 
+        url)`);
+      console.log(products);
+    };
+    getProducts();
+  }, []);
+  return <div>Products</div>;
+};
+
+export default Products;
---
+++ package/trade-sphere/lib/constants.ts
@@ -0,0 +1,4 @@
+export const TABLES = {
+  product: "products",
+  image: "images",
+};
